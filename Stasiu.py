5bd2dd2f71a955ff10f8192bf1bf40c781fbbe82
Gabriela Marek <148369562+GabrielaMarek@users.noreply.github.com>
2025-01-08 12:33:21 +0100

    runtime error

--- Stasiu.py
+++ Stasiu.py
@@ -1,6 +1,7 @@
 from highlight_text_with_arrows import *
 
 #ERRORS
+
 class Error:
     def __init__(self, pos_start, pos_end, error_name, details):
         self.pos_start = pos_start
@@ -21,7 +22,33 @@ class WrongSyntaxError(Error):
 class CharacterFormatError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Oops! It looks like there is a character that is ILLEGAAAAAAAL!', details)
-        
+
+class RuntimeError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, 'Runtime Error', details)
+        self.context = context
+
+    def as_string(self):
+        result = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}\n'
+        result += '\n' + highlight_text_with_arrows(self.pos_start.fulltxt, self.pos_start, self.pos_end)
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+
+        while ctx:
+            result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+
+        return 'Traceback (most recent call last):\n' + result
+
+
+
+    
 
 #POSITION
 
@@ -434,7 +461,6 @@ class Parser:
         return self.bin_op(self.term, (TOKTYPE_PLUS, TOKTYPE_MINUS))
 
 
-
     def bin_op(self, func, ops):
         res = ResultOfParse()
         left = res.register(func())
@@ -519,13 +545,13 @@ class Number:
         elif op == "div":
             if other.value == 0:
                 return None, RuntimeError(
-                    other.pos_start, other.pos_end, "Division by zero", self.context
+                    other.pos_start, other.pos_end, "Bruh.. You can't divide by 0...", self.context
                 )
             return Number(self.value / other.value, self.pos_start, other.pos_end, self.context), None
         elif op == "pow":
             return Number(self.value ** other.value, self.pos_start, other.pos_end, self.context), None
         else:
-            raise ValueError("Unsupported operation")
+            raise ValueError("Unsupported operation :(")
 
     def added_to(self, other):
         return self.operate(other, "add")
@@ -552,6 +578,7 @@ class Context:
         self.display_name = display_name
         self.parent = parent
         self.parent_entry_pos = parent_entry_pos
+
 
 #INTERPRETER
 
