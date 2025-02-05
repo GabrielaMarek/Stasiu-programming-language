3423f415108e7546066652e369c1c0b7e3b6fc27
Gabriela Marek <148369562+GabrielaMarek@users.noreply.github.com>
2025-02-05 13:02:13 +0100

    display fix

--- Stasiu.py
+++ Stasiu.py
@@ -40,7 +40,7 @@ class RuntimeError(Error):
         ctx = self.context
 
         while ctx:
-            result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            result = f'  File {pos.filename}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
             pos = ctx.parent_entry_pos
             ctx = ctx.parent
 
@@ -1132,8 +1132,10 @@ class Interpreter:
         value = res.register(self.visit(node.value, context))
         if res.error: 
             return res
-
-        print(value) 
+        if isinstance(value, String):
+            print(value.value)  
+        else:
+            print(value)
         return res.success(None)
 
 
